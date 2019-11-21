9dd95b64bcf7437675bb32723bc97a1a995f371f
Dmitry Zhbankov <dmitry.zhbankov@gmail.com>
2019-11-21 12:45:06 +0300

    Add code

---
+++ SingletonApp/Classes/DbItem.cs
@@ -0,0 +1,8 @@
+﻿namespace SigletonApp
+{
+    class DbItem
+    {       
+        public int Id { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ SingletonApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SigletonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppContext appContext = AppContext.GetInstance();                  
+            appContext.Database.EnsureCreated();
+            appContext.Items.Add(new DbItem() { Content = "first item" });
+            appContext.Items.Add(new DbItem() { Content = "second item" });
+            var count = appContext.SaveChanges();
+            Console.WriteLine("{0} records saved to database", count);
+            Console.WriteLine();
+            Console.WriteLine("All items in database:");
+            foreach (var item in appContext.Items)
+            {
+                Console.WriteLine(" - {0}", item.Content);
+            }
+            Console.WriteLine();
+            AppContext appContext2 = AppContext.GetInstance();
+            if (appContext==appContext2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+            appContext.Database.EnsureCreated();
+            appContext.Items.Add(new DbItem() { Content = "third item" });
+            appContext.Items.Add(new DbItem() { Content = "fourth item" });
+            count = appContext.SaveChanges();
+            Console.WriteLine("{0} records saved to database", count);
+            Console.WriteLine();
+            Console.WriteLine("All items in database:");
+            foreach (var item in appContext.Items)
+            {
+                Console.WriteLine(" - {0}", item.Content);
+            }
+            Console.ReadLine();
+        }
+    }
+}
